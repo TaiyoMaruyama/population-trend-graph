74c07aecdbc155dc4ab528a79f2d783efbf8a547
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2024-10-19 19:13:56 +0900

    themeの定義

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,22 @@
+'use client';
+
+import { ThemeProvider } from '@emotion/react';
+import GlobalStyles from '@/components/common/GlobalStyles';
+import theme from '@/themes/theme';
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='ja'>
-      <body>{children}</body>
-    </html>
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <html lang='ja'>
+          <body>{children}</body>
+        </html>
+      </ThemeProvider>
+    </>
   );
 }
---
+++ src/themes/theme.ts
@@ -0,0 +1,13 @@
+import breakpoints from './config/breakpoint';
+import colors from './config/colors';
+import fonts from './config/fonts';
+import spacing from './config/spacing';
+
+const theme = {
+  colors: colors,
+  fonts: fonts,
+  spacing: spacing,
+  breakpoints: breakpoints,
+};
+
+export default theme;
