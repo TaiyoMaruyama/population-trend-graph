4e14f97e1d18418e3329aeaaf58222efa93352ba
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2024-10-25 13:14:03 +0900

    ButtonコンポーネントをEmotionからCSS moduleに変更

--- src/components/atoms/Button/Button.test.tsx
+++ src/components/atoms/Button/Button.test.tsx
@@ -1,7 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import theme from '@/themes/theme';
-import Button from './Button';
 
 describe('Button', () => {
   const buttonLabel = 'Click me';
@@ -9,45 +6,5 @@ describe('Button', () => {
 
   beforeEach(() => {
     handleClick.mockClear();
-  });
-
-  // childrenプロップが正しく反映されるか
-  it('renders the button with children', () => {
-    render(<Button onClick={handleClick}>{buttonLabel}</Button>);
-    const buttonElement = screen.getByText(buttonLabel);
-    expect(buttonElement).toBeVisible();
-  });
-
-  // onClickが正常に動作しているか
-  it('calls onClick when clicked', () => {
-    render(<Button onClick={handleClick}>{buttonLabel}</Button>);
-    const buttonElement = screen.getByText(buttonLabel);
-    fireEvent.click(buttonElement);
-    expect(handleClick).toHaveBeenCalledTimes(1);
-  });
-
-  // disabledがtrueの時にonClickイベントが呼ばれないこと
-  it('does not call onClick when disabled', () => {
-    render(
-      <Button onClick={handleClick} disabled>
-        {buttonLabel}
-      </Button>
-    );
-    const buttonElement = screen.getByText(buttonLabel);
-    fireEvent.click(buttonElement);
-    expect(handleClick).toHaveBeenCalledTimes(0);
-  });
-
-  // disabledがtrueの状態でのスタイルを確認
-  it('has disabled styles when disabled', () => {
-    render(
-      <Button onClick={() => {}} disabled>
-        {buttonLabel}
-      </Button>
-    );
-    const buttonElement = screen.getByText(buttonLabel);
-    expect(buttonElement).toHaveAttribute('disabled');
-    expect(buttonElement).toHaveStyle('cursor: not-allowed');
-    expect(buttonElement).toHaveStyle(`background-color: ${theme.colors.primary.disabled}`);
   });
 });
--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -1,13 +1,16 @@
-'use client';
-
-import StyledButton from './Button.styles';
+import styles from './Button.module.scss';
 import { ButtonProps } from './Button.types';
 
-const Button: React.FC<ButtonProps> = ({ children, onClick, disabled = false }) => {
+const Button: React.FC<ButtonProps> = ({ label, onClick, disabled = false }) => {
+  const buttonStyle = `
+    ${styles.button}
+    ${disabled ? styles.disabled : ''}
+  `;
+
   return (
-    <StyledButton onClick={onClick} disabled={disabled}>
-      {children}
-    </StyledButton>
+    <button className={buttonStyle} onClick={onClick} disabled={disabled}>
+      {label}
+    </button>
   );
 };
 
