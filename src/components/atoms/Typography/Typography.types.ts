d1c1037a35543b63e29685c23219d95315ee012e
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2024-10-26 17:30:15 +0900

    TypographyコンポーネントをEmotionからCSS moduleに変更

--- src/components/atoms/Typography/Typography.types.ts
+++ src/components/atoms/Typography/Typography.types.ts
@@ -1,10 +1,7 @@
-type HeadingVariant = keyof JSX.IntrinsicElements & ('h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6');
+type HeadingVariant = keyof JSX.IntrinsicElements & ('h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p');
 
-export interface TypographyStyleProps {
-  bold?: true;
+export interface TypographyProps {
+  text: string;
+  bold?: boolean;
   variant?: HeadingVariant;
-}
-
-export interface TypographyProps extends TypographyStyleProps {
-  children: React.ReactNode;
 }
