bdb4d496bbad7237b507d971b7c083a49a0ced1c
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2024-10-26 16:04:29 +0900

    TabコンポーネントをEmotionからCSS moduleに変更

--- src/components/atoms/Tab/Tab.tsx
+++ src/components/atoms/Tab/Tab.tsx
@@ -1,13 +1,13 @@
-'use client';
-
-import { StyledTab } from './Tab.styles';
+import styles from './Tab.module.scss';
 import { TabProps } from './Tab.types';
 
 const Tab: React.FC<TabProps> = ({ label, selected, onClick }) => {
+  const tabStyle = `${styles.tab} ${selected ? styles.selected : ''}`;
+
   return (
-    <StyledTab selected={selected} onClick={onClick}>
+    <button className={tabStyle} onClick={selected ? undefined : onClick}>
       {label}
-    </StyledTab>
+    </button>
   );
 };
 
