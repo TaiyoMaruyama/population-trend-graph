337912bd2d77cbf83fafedf4eb310b679f1c8d7c
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2024-10-26 23:38:53 +0900

    ARIA属性の追加と選択済みのタブにフォーカスが当たらないように修正

--- src/components/atoms/Tab/Tab.tsx
+++ src/components/atoms/Tab/Tab.tsx
@@ -5,7 +5,13 @@ const Tab: React.FC<TabProps> = ({ label, selected, onClick }) => {
   const tabStyle = `${styles.tab} ${selected ? styles.selected : ''}`;
 
   return (
-    <button className={tabStyle} onClick={selected ? undefined : onClick}>
+    <button
+      className={tabStyle}
+      onClick={selected ? undefined : onClick}
+      aria-disabled={selected}
+      aria-label={label}
+      tabIndex={selected ? -1 : 0}
+    >
       {label}
     </button>
   );
