9b6a4e9fd9bccb853ac4b4c6a671d80b3f5cf417
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2024-10-28 23:49:57 +0900

    PrefSelectionFrameコンポーネントに対するStoryを作成

---
+++ src/components/organisms/PrefSelectionFrame/PrefSelectionFrame.stories.tsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import { Prefecture } from '@/types';
+import PrefSelectionFrame from './PrefSelectionFrame';
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta = {
+  title: 'organisms/PrefSelectionFrame',
+  component: PrefSelectionFrame,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  render: () => {
+    const [checkedPrefectures, setCheckedPrefectures] = useState<Prefecture[]>([]);
+
+    return (
+      <div style={{ padding: '20px' }}>
+        <PrefSelectionFrame
+          checkedPrefectures={checkedPrefectures}
+          setCheckedPrefectures={setCheckedPrefectures}
+        />
+      </div>
+    );
+  },
+};
+
+export const WithCheckedPrefectures: Story = {
+  render: () => {
+    const initialCheckedPrefectures: Prefecture[] = [
+      { prefCode: 1, prefName: '北海道' },
+      { prefCode: 2, prefName: '青森県' },
+    ];
+
+    const [checkedPrefectures, setCheckedPrefectures] =
+      useState<Prefecture[]>(initialCheckedPrefectures);
+
+    return (
+      <div style={{ padding: '20px' }}>
+        <PrefSelectionFrame
+          checkedPrefectures={checkedPrefectures}
+          setCheckedPrefectures={setCheckedPrefectures}
+        />
+      </div>
+    );
+  },
+};
