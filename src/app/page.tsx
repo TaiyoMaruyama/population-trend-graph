1c5858c2f6bbca2d31024641564b8a70af815b8c
TaiyoMaruyama <stickytomatobaby0501@gmail.com>
2024-10-29 03:46:09 +0900

    misc : page.tsxのコードを削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,1 @@
-'use client';
-
-import { useState } from 'react';
-import PrefSelectionFrame from '@/components/organisms/PrefSelectionFrame/PrefSelectionFrame';
-import { Prefecture } from '@/types';
-
-export default function Home() {
-  const [checkedPrefectures, setCheckedPrefectures] = useState<Prefecture[]>([]);
-  return (
-    <PrefSelectionFrame
-      checkedPrefectures={checkedPrefectures}
-      setCheckedPrefectures={setCheckedPrefectures}
-    />
-  );
-}
+export default function Home() {}
